e43ef8d2ff0b3d18c950b1f4536c87bbf3dad99f
Norton Pengra <npengra317@gmail.com>
2016-09-12 15:30:06 -0700

    updates for sql

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var dialog = require('./dialog.js');
+var https = require('https');
+var querystring = require('querystring');
+var sql = require('./sql');
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function() {
@@ -25,5 +28,29 @@ bot.dialog('/', [
     },
     function(session, results) {
         session.send(results.response);
+        var playername = results.response;
+        var path = '/indexes/playername/docs?api-version=2015-02-28&api-key=A1E4623A5329B55605CDE0380822AE57&search=';
+        path += querystring.escape(playername);
+        loadData(path, function(players) {
+            var displayName = players.value[0].displayName;
+            sql.getPlayerData(displayName);
+        });
     }
 ]);
+
+function loadData(path, callback) {
+    var options = {
+        host: 'nflbot.search.windows.net',
+        port: 443,
+        path: path,
+        method: 'GET'
+    };
+    var request = https.request(options, function (response) {
+        var data = '';
+        response.on('data', function (chunk) { data += chunk; });
+        response.on('end', function () {
+            callback(JSON.parse(data));
+        });
+    });
+    request.end();
+} 
--- sql.js
+++ sql.js
@@ -1,22 +1,27 @@
-var sql = require('seriate');
-sql.setDefault({
+var wrapper = function () {
+    var self = this;
+    self.sql = require('seriate');
+    self.sql.setDefaultConfig({
         user: "nflbot",
         password: "Durango123!",
         host: "nflbot.database.windows.net",
         database: "nflbot",
         options: {
-            encrypt: true  
+            encrypt: true
         }
-    }); 
-var player = 'Philip Rivers';
-var getPlayerData = function(player){
-    sql.execute( {  
-        query: "SELECT * from TeamPlayer where displayName = '" + player + "'"
-    } ).then( function( res ) {
-        results = res;
-        console.log(results);
-        module.exports = results; 
-    }, function( err ) {
-        console.log( "Something bad happened:", err );
-    } );
+    });
+
+    self.getPlayerData = function (player) {
+        console.log(self.sql)
+        self.sql.execute({
+            query: "SELECT * from TeamPlayer where displayName = '" + player + "'"
+        }).then(function (res) {
+            results = res;
+            console.log(results);
+        }, function (err) {
+            console.log("Something bad happened:", err);
+        });
+    };
 };
+
+module.exports = new wrapper();
