9eb7a232a18e2d47a57875c10fe52ce335f513b0
Christopher Harrison <chrhar@microsoft.com>
2016-09-14 18:03:14 -0700

    updates to prompt flow

--- server.js
+++ server.js
@@ -54,12 +54,18 @@ var connector = new builder.ChatConnector({
     // appId: process.env.MICROSOFT_APP_ID,
     // appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 let dialog = new builder.IntentDialog()
     .onDefault((session, args, next) => {
-        builder.Prompts.choice(session, 'What would you like to do?', ['Get Stats']);
+        let buttons = [new builder.CardAction.imBack(session, 'get stats', 'Get Stats')]
+        let message = new builder.HeroCard(session).title('What would you like to do?').buttons(buttons);
+
+        builder.Prompts.choice(session, message, ['Get Stats']);
+
+        // builder.Prompts.choice(session, 'What would you like to do?', ['Get Stats']);
         next(); // end the dialog
     })
     .matches(/^get ?stats$/u, [
